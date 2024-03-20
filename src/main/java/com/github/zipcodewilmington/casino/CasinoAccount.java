4caddcaaa8bad85e4e34cd422f778d0fb37614b2
Chaz-Adams <caadams1922@gmail.com>
2024-03-20 18:42:16 -0400

    updated some roulette tests

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -1,17 +1,27 @@
 package com.github.zipcodewilmington.casino;
 
+import java.util.Date;
+
 /**
  * Created by leon on 7/21/2020.
  * `ArcadeAccount` is registered for each user of the `Arcade`.
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
+    public CasinoAccount(String firstName, String lastName, Date birthdate, String userName, String password) {
+    }
 
-    // userName
-    // userPass
-    // balance
-    // checkBalc()
-    // addBalc(0)
-    // withBalc()
+    public String getFirstName() {
+        return null;
+    }
+
+    public String getLastName() {
+        return null;
+    }
+
+    public String getBirthDate() {
+        return null;
+    }
+
 
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/Trivia/TriviaGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Trivia/TriviaGame.java
@@ -1,4 +1,7 @@
 package com.github.zipcodewilmington.casino.games.Trivia;
 
 public class TriviaGame {
+    public static boolean startGame() {
+        return true;
+    }
 }
